9e7621c600705be323a6e6844f825b8bca53f96e
Kevinjose102 <kevinjose2505@gmail.com>
2024-04-20 09:45:20 +0530

    updated the features

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const container = document.querySelector(".container");
 const gridButton = document.querySelector(".grid-size")
 const clearButton = document.querySelector(".clear");
+const colorModeButton = document.querySelector(".color-mode");
+const rainbowModeButton = document.querySelector(".rainbow-mode");
+const eraserButton = document.querySelector(".eraser");
 
+let DEFAULT_COLOR = "#333333";
+let DEFAULT_SIZE = 30;
+let currentMode = "color-mode";
+let currentColor = DEFAULT_COLOR;
 let dimenstionString ="";
-let size = 30;
+let size = DEFAULT_SIZE;
+
 //fucntion to make the grid of a given size
 function makeGrid(size){
     
@@ -21,7 +29,6 @@ function makeGrid(size){
 
         gridBox.addEventListener("mouseover", changeColor);
         gridBox.addEventListener("mousedown", changeColor);
-
     }
 }
 
@@ -31,14 +38,25 @@ document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
 //creates the default grid
-makeGrid(30);
+makeGrid(DEFAULT_SIZE);
 
 //changes the color of the grid element when the mouse is down and dragged over
 function changeColor(e){
+    console.log(currentMode)
     //mouse is down and it is hovering
     if (e.type === 'mouseover' && !mouseDown) return
-    else{
-        e.target.style.backgroundColor = "black";
+    if(currentMode === "color-mode"){
+        currentColor = document.getElementById("color-picker").value
+        e.target.style.backgroundColor = currentColor;
+    }
+    if(currentMode === "rainbow-mode"){
+        const randomR = Math.floor(Math.random() * 256)
+        const randomG = Math.floor(Math.random() * 256)
+        const randomB = Math.floor(Math.random() * 256)
+        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    }
+    if(currentMode === "eraser"){
+        e.target.style.backgroundColor = "white";
     }
 }
 
@@ -64,6 +82,19 @@ gridButton.addEventListener("click", () => {
         makeGrid(size);
     }
     else{
-        size = 30;
+        size = DEFAULT_SIZE;
     }
+})
+
+//DIFFERENT MODES
+colorModeButton.addEventListener("click", () => {
+    currentMode = "color-mode";
+})
+
+rainbowModeButton.addEventListener("click", () => {
+    currentMode = "rainbow-mode";
+})
+
+eraserButton.addEventListener("click", () => {
+    currentMode = "eraser";
 })
